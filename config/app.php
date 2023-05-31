6276044f26bf10dadea16c7808b383f1ddb447cb
Berkay Kaya <62906693+bberkay@users.noreply.github.com>
2023-05-31 05:06:42 +0300

    Update app.php

--- config/app.php
+++ config/app.php
@@ -55,7 +55,8 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    //'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://laravel-livewire-ui.onrender.com'),
 
     'asset_url' => env('ASSET_URL'),
 
